6c5391478e296d5b09e4015858741c2a39faae6f
orest-forest <danilchenko.orest@gmail.com>
2020-01-27 11:29:39 +0200

    add files

---
+++ src/components/weather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+                { this.props.city &&
+                    <div>
+                <p>Местоположение:{this.props.city}, {this.props.country} </p>
+                <p>Температура:{this.props.temp} </p>
+                        <p>Давление: {this.props.pressure}</p>
+                    <p>Восход солнца:{this.props.sunrise} </p>
+                    <p>Заход солнца:{this.props.sunset} </p>
+                    </div>
+                }
+
+            </div>
+        );
+    }
+}
+
+export default Weather;
